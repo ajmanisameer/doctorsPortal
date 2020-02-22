89a1e54e7a96c948a7f5d9a23c96b1ede70337f4
Ajmani <ajmani@Schools-Mac-mini-2.local>
2020-02-22 10:38:23 +0530

    initial

---
+++ app.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const flash = require('connect-flash');
+const session = require('express-session');
+const multer = require('multer');
+const	upload = require('./routes/upload');
+
+const app = express();
+
+uploader = multer({
+	dest: 'uploads/'
+});
+
+// Passport Config
+require('./config/passport')(passport);
+
+
+// Connect to MongoDB
+
+mongoose.connect('mongodb+srv://aj:ajmani@cluster0-c60su.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Database Connected'));
+
+// EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+// Express body parser
+app.use(express.urlencoded({ extended: true }));
+
+// Express session
+app.use(
+  session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect flash
+app.use(flash());
+
+// Global variables
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
+
+// Routes
+app.post('/upload', uploader.single('singleFile'), upload.s3); //"singleFile" is the field name
+// app.use('/upload', require('./routes/upload'))
+app.use('/', require('./routes/index.js'));
+app.use('/users', require('./routes/users.js'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
---
+++ routes/upload.js
@@ -0,0 +1,41 @@
+var _ = require('underscore'),
+AWS = require('aws-sdk'),
+fs = require('fs'),
+path = require('path'),
+flow = require('flow');
+
+configPath = path.join(__dirname, '..', "config.json");
+
+AWS.config.loadFromPath(configPath);
+
+exports.s3 = function(req, res) {
+var s3 = new AWS.S3(),
+    file = req.file,
+    result = {
+        error: 0,
+        uploaded: []
+    };
+
+flow.exec(
+    function() { // Read temp File
+        fs.readFile(file.path, this);
+    },
+    function(err, data) { // Upload file to S3
+        s3.putObject({
+            Bucket: 'medical-images', //Bucket Name
+            Key: file.originalname, //Upload File Name, Default the original name
+            Body: data
+        }, this);
+    },
+    function(err, data) { //Upload Callback
+        if (err) {
+            console.error('Error : ' + err);
+            result.error++;
+        }
+        result.uploaded.push(data.ETag);
+        this();
+    },
+    function() {
+        res.send({msg: "Uploaded"});
+    });
+};
